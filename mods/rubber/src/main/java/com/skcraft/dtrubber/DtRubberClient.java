81dbd3f81b2ad61dda5930b9619656efcfacbed2
Magi1053 <magi1053@outlook.com>
2026-08-06 22:22:13 -0500

    Drop deprecated EventBusSubscriber.Bus usage on client listeners.

--- mods/rubber/src/main/java/com/skcraft/dtrubber/DtRubberClient.java
+++ mods/rubber/src/main/java/com/skcraft/dtrubber/DtRubberClient.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
-@EventBusSubscriber(modid = DtRubber.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = DtRubber.MOD_ID, value = Dist.CLIENT)
 public final class DtRubberClient {
     private DtRubberClient() {}
 
